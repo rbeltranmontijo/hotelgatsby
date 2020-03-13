70ed24d202c583f0a705f8417e21fc1234aaabfc
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-12 21:50:28 -0600

    plugins, algunos componentes y estilos globales

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react"
+import Helmet from "react-helmet"
+import { Global, css } from "@emotion/core"
 
-const Layout= () => {
-    retrun (
-        <h1>Desde Layout</h1>
-    )
+import Header from "../components/header"
+
+const Layout = props => {
+  return (
+    <>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+          }
+          body {
+            font-size: 16px;
+            font-sie: 1.6rem;
+            line-height: 1.5;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,
+          h2 {
+            font-family: "Roboto", serif;
+          }
+          h3 {
+            font-family: "PT Sans", sans-serif;
+          }
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+        <link
+          href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <Header />
+      {props.children}
+    </>
+  )
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <h1>Gatsby Hotel</h1>
+  <Layout>
+    <h1>Gatsby Hotel</h1>
+  </Layout>
 )
 
 export default IndexPage
