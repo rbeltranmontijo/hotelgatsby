ee98e562af2a67b315393263e9b1064f61b17713
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-12 21:02:55 -0600

    iniciando

---
+++ src/components/layout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Layout= () => {
+    retrun (
+        <h1>Desde Layout</h1>
+    )
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <h1>Gatsby Hotel</h1>
+)
+
+export default IndexPage
